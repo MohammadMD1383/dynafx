63d7b9ace983160f0c8a18cd3305400f3cbbd815
Mohammad Mostafa <ezmmd83@gmail.com>
2023-11-28 14:04:57 +0330

    created project template

---
+++ app/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace app.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+    public string Greeting => "Welcome to Avalonia!";
+#pragma warning restore CA1822 // Mark members as static
+}
